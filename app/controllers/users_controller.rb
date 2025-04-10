15cdfc5a7c9a432d1de74b32d57ca9d7fecba93b
Gustavo D. Santana <gustavod.santana@gmail.com>
2025-04-10 19:08:03 -0300

    Adição de admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:users]
   respond_to :json
 
   def index
@@ -13,9 +13,18 @@ class UsersController < ApplicationController
     user = User.find_by(id: params[:id])
 
     if user.nil?
-      render json: {message: "Not found"}, status: 404
+      render json: { message: "Not found"}, status: 404
     else
       render json: UserSerializer.new(user).serializable_hash[:data][:attributes]
     end
+  end
+
+  def update
+    user = User.find(params[:id])
+      if user.update_attributes(params[:user])
+        render json: { }, status: :ok
+      else
+        render json: {}, status: :unprocessable_entity
+      end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+  }, path: '/admin', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout'
+  }
   get 'current_user', to: 'current_user#index'
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
